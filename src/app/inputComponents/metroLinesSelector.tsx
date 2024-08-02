100d5ca33d43e510f3660c3da10a6f43d4a77ea8
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2024-08-02 09:38:03 -0700

    Convert line selector to table

--- src/app/inputComponents/metroLinesSelector.tsx
+++ src/app/inputComponents/metroLinesSelector.tsx
@@ -29,54 +29,64 @@ export default function LineSelector({
   return (
     <div id="lineSelector" className="bg-white p-4 rounded-xl">
       <div className="flex gap-4 items-center mb-8">
-        <span className="text-sm uppercase">Line Selector</span>
+        <span className="text-sm uppercase whitespace-nowrap">
+          Line Selector
+        </span>
         <button className="text-neutral-400 text-sm">Expand</button>
       </div>
 
-      <ul className="max-h-screen overflow-y-auto">
-        {(lines as Line[]).map((line, index) => {
-          return (
-            <li
-              key={index}
-              className="flex gap-2 items-center px-2 odd:bg-neutral-50 text-sm"
-            >
-              <Checkbox.Root
-                id={line.line.toString()}
-                onClick={() => {
-                  const selectedLinesCopy = [...selectedLines];
-
-                  // Update checkbox value
-                  if (selectedLinesCopy.includes(line.line.toString())) {
-                    const pos = selectedLinesCopy.indexOf(line.line.toString());
-
-                    selectedLinesCopy.splice(pos, 1);
-                  } else {
-                    selectedLinesCopy.push(line.line.toString());
-                  }
-
-                  // Update local state
-                  setSelectedLines(selectedLinesCopy);
-                }}
-                checked={selectedLines.includes(line.line.toString())}
-                className="flex items-center justify-center bg-white data-[state=checked]:bg-neutral-500 border border-neutral-500 rounded-lg h-5 w-5 overflow-hidden"
+      <table className="max-h-screen overflow-y-auto w-full">
+        <tbody>
+          {(lines as Line[]).map((line, index) => {
+            return (
+              <tr
+                key={index}
+                className="flex gap-2 items-center px-2 odd:bg-neutral-50 text-sm"
               >
-                <Checkbox.Indicator className="bg-neutral-500 rounded-lg h-full w-full" />
-              </Checkbox.Root>
+                <td>
+                  <Checkbox.Root
+                    id={line.line.toString()}
+                    onClick={() => {
+                      const selectedLinesCopy = [...selectedLines];
 
-              <label
-                htmlFor={String(line.line)}
-                className="flex-1 cursor-pointer py-2"
-              >
-                {line.mode === 'Bus'
-                  ? `Line ${line.line}`
-                  : line.mode === 'Rail'
-                    ? `${railLetters.get(line.line)} Line`
-                    : ''}
-              </label>
-            </li>
-          );
-        })}
-      </ul>
+                      // Update checkbox value
+                      if (selectedLinesCopy.includes(line.line.toString())) {
+                        const pos = selectedLinesCopy.indexOf(
+                          line.line.toString(),
+                        );
+
+                        selectedLinesCopy.splice(pos, 1);
+                      } else {
+                        selectedLinesCopy.push(line.line.toString());
+                      }
+
+                      // Update local state
+                      setSelectedLines(selectedLinesCopy);
+                    }}
+                    checked={selectedLines.includes(line.line.toString())}
+                    className="flex items-center justify-center bg-white data-[state=checked]:bg-neutral-500 border border-neutral-500 rounded-lg h-5 w-5 overflow-hidden"
+                  >
+                    <Checkbox.Indicator className="bg-neutral-500 rounded-lg h-full w-full" />
+                  </Checkbox.Root>
+                </td>
+
+                <td className="w-full">
+                  <label
+                    htmlFor={String(line.line)}
+                    className="flex-1 block cursor-pointer py-2"
+                  >
+                    {line.mode === 'Bus'
+                      ? `Line ${line.line}`
+                      : line.mode === 'Rail'
+                        ? `${railLetters.get(line.line)} Line`
+                        : ''}
+                  </label>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
