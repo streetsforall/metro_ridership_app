79e55983e1e7d61085ffb1c0f12d51781cf912c6
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2025-06-20 13:53:05 -0700

    Add back future data structure

---
+++ src/data/future-data-structure.js
@@ -0,0 +1,47 @@
+const line1 = [
+  {
+    year: 2020,
+    month: 1,
+    wkdy: 4000,
+    sat: 2400,
+    sun: 2400,
+  },
+  {
+    year: 2020,
+    month: 2,
+    wkdy: 4000,
+    sat: 2400,
+    sun: 2400,
+  },
+  {
+    year: 2020,
+    month: 3,
+    wkdy: 4000,
+    sat: 2400,
+    sun: 2400,
+  },
+];
+
+const line2 = [
+  {
+    year: 2020,
+    month: 1,
+    wkdy: 4000,
+    sat: 2400,
+    sun: 2400,
+  },
+  {
+    year: 2020,
+    month: 2,
+    wkdy: 4000,
+    sat: 2400,
+    sun: 2400,
+  },
+  {
+    year: 2020,
+    month: 3,
+    wkdy: 4000,
+    sat: 2400,
+    sun: 2400,
+  },
+];
